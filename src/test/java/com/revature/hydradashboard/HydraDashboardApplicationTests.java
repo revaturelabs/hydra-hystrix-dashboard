837b2221518fef3e364f40cbf0288d9fa8178550
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-16 10:37:44 -0400

    Init.

---
+++ src/test/java/com/revature/hydradashboard/HydraDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.revature.hydradashboard;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class HydraDashboardApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
