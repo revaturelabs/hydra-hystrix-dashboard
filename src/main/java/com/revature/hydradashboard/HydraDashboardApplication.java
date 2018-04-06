1a14a33573245e5825a76f507b1b6aa02d1c59d7
JohnBRevature <john.b.brand.jr@gmail.com>
2018-04-06 09:41:56 -0400

    Add one java doc of @author unknown, and short description

--- src/main/java/com/revature/hydradashboard/HydraDashboardApplication.java
+++ src/main/java/com/revature/hydradashboard/HydraDashboardApplication.java
@@ -2,11 +2,14 @@ package com.revature.hydradashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 
+ * @author unknown
+ *	Circuit Breaker
+ */
 
 @SpringBootApplication
-
 public class HydraDashboardApplication {
 
 	public static void main(String[] args) {
